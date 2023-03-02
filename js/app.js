e085439cb27ee7e4f902fdb73a692cab86b2a2e1
Tarik Habhab <t.habhab811@gmail.com>
2023-03-02 13:01:07 -0600

    car

---
+++ js/app.js
@@ -0,0 +1,29 @@
+"use strict";
+console.log("proof of life, so that know our js is connected up.");
+
+// Add at least 3 dynamic components to your web page. These can be based on user input, or other dynamic input data.
+
+// At least one dynamic component should be based on user input.
+
+// At least one dynamic component should use a conditional statement to determine the output.
+
+
+let userName = prompt("What is your name?");
+console.log("user name: ", userName);
+
+let confirmMessage = confirm("Do you want to continue?");
+console.log("confirm message: ", confirmMessage);
+
+let userCar = prompt("Whats your favorite car manufacturer");
+console.log("car manufacturer: ", userCar);
+
+if (userCar === "porsche") 
+{
+    response = "good taste";
+}
+
+// console.log("greeting", greeting);
+
+// return greeting;
+
+// document.write("Hello " + userName + " " + greeting);
