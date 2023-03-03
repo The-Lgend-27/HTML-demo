f90660c2a10261c8c3ceaa8a57b4553c7e6e724d
Tarik Habhab <t.habhab811@gmail.com>
2023-03-03 09:22:49 -0600

    image vaildation

--- js/app.js
+++ js/app.js
@@ -4,36 +4,33 @@ console.log("proof of life, so that know our js is connected up.");
 let userName = prompt("What is your name?");
 console.log("user name: ", userName);
 let confirmMessage = confirm("Do you want to continue?");
-console.log("confirm message: ", confirmMessage);  
-let userCar = prompt("Whats your favorite car manufacturer").toLowerCase();
-console.log("🚀 ~ file: app.js:9 ~ userCar:", userCar);
+console.log("confirm message: ", confirmMessage);
 
-//JEEP
+function getCarType() {
+    let userCar = prompt("Whats your favorite car manufacturer").toLowerCase();
+    console.log("🚀 ~ file: app.js:9 ~ userCar:", userCar);
+    let response;
 
-                    //JEEP making it lowercase to compare the right value in our if / else conditional.
-// let lowerCaseCar = userCar.toLowerCase();
-
-// console.log('lower case? ',lowerCaseCar);
-
-
-//declare variable so that it has a space in memory to store the response.
-
-let response;
-
-if (userCar === "porsche") {
-  response = "good taste";
-} else if (userCar === "jeep") {
-  response = "cool I like jeeps.";
-} else if (userCar === "tesla") {
-  response = "electric cars are futuristic";
-} else {
-  response = "I love bikes!";
+    if (userCar === "porsche") {
+        response = "good taste";
+    } else if (userCar === "jeep") {
+        response = "cool I like jeeps.";
+    } else if (userCar === "tesla") {
+        response = "electric cars are futuristic";
+    } else {
+        response = "I love bikes!";
+    }
+    console.log("response", response);
+    return response;
 }
+getCarType();
 
-console.log("response", response);
 
-// console.log("greeting", greeting);
+let imageElement = document.getElementById("imageSection");
+console.log(imageElement);
 
-// return greeting;
+let userInput = prompt("How many images would you like to see?");
+console.log("🚀 ~ file: app.js:40 ~ userInput:", typeof userInput);
 
-// document.write("Hello " + userName + " " + greeting);
+let userInputParsed = parseInt(userInput);
+console.log("🚀 ~ file: app.js:43 ~ userInputParsed:", userInputParsed);
